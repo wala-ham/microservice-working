f51a4fab705a9a163b929faac4d9fae12d28eba7
wala-ham <wala.hammemi00@gmail.com>
2023-06-22 11:30:53 +0200

    kafka trying

--- order-service/index.js
+++ order-service/index.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT_ONE || 9090;
+const PORT = process.env.PORT_ONE || 9090 ;
 const mongoose = require("mongoose");
 const Order = require("./Order");
 const amqp = require("amqplib");
 const isAuthenticated = require("./isAuthenticated");
+const kafka = require('kafka-node');
+const Consumer = kafka.Consumer;
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const producer = kafka.producer();
+const { Kafka } = require('kafkajs');
+
+
+const consumerOptions = {
+  groupId: 'my-consumer-group',
+  autoCommit: true,
+  autoCommitIntervalMs: 5000,
+  sessionTimeout: 15000,
+  fetchMaxBytes: 10 * 1024 * 1024, // 10 MB
+  fromOffset: 'latest'
+};
+
+const topics = ['ORDER','PRODUCT'];
+const consumer = new Consumer(client, topics, consumerOptions);
+const kafka = new Kafka({
+  clientId: 'my-producer',
+  brokers: ['localhost:9092'],
+});
+
 
 var channel, connection;
 
+
+// setup mongodb
 mongoose.connect(
    "mongodb+srv://admin:admin@cluster0.9gcsd.mongodb.net/microservices-GSS",
     {
@@ -34,13 +59,38 @@ function createOrder(products, userEmail) {
     return newOrder;
 }
 
-async function connect() {
+/* async function connect() {
     const amqpServer = "amqp://localhost:5672";
     connection = await amqp.connect(amqpServer);
     channel = await connection.createChannel();
     await channel.assertQueue("ORDER");
-}
-connect().then(() => {
+} */
+
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'ORDER', fromBeginning: true });
+  
+    await consumer.run({
+      eachMessage: async ({ topic, data }) => {
+        console.log("Consuming ORDER service");
+        const { products, userEmail } = JSON.parse(data.content);
+        const newOrder = createOrder(products, userEmail);
+       
+        producer.connect();
+        const message = { 
+              value: JSON.stringify({ newOrder }),
+            };
+        producer.send({
+              topic: 'PRODUCT',
+              messages: [message],
+            });
+        producer.disconnect();
+          
+      },
+    });
+  
+  
+
+/* connect().then(() => {
     channel.consume("ORDER", (data) => {
         console.log("Consuming ORDER service");
         const { products, userEmail } = JSON.parse(data.content);
@@ -51,7 +101,7 @@ connect().then(() => {
             Buffer.from(JSON.stringify({ newOrder }))
         );
     });
-});
+}); */
 
 app.listen(PORT, () => {
     console.log(`Order-Service at ${PORT}`);
--- product-service/index.js
+++ product-service/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT_ONE || 8080 ;
+const PORT = process.env.PORT_ONE || 8080;
 const mongoose = require("mongoose");
 const Product = require("./Product");
 const jwt = require("jsonwebtoken");
 const amqp = require("amqplib");
 const isAuthenticated = require("./isAuthenticated");
+const kafka = require('kafka-node');
+const Consumer = kafka.Consumer;
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const producer = kafka.producer();
+const { Kafka } = require('kafkajs');
+
+const topics = ['ORDER', 'PRODUCT'];
+const consumer = new Consumer(client, topics, consumerOptions);
+const kafka = new Kafka({
+    clientId: 'my-producer',
+    brokers: ['localhost:9092'],
+});
+
+
 var order;
 
 var channel, connection;
@@ -22,33 +36,42 @@ mongoose.connect(
     }
 );
 
-async function connect() {
+/* async function connect() {
     const amqpServer = "amqp://localhost:5672";
     connection = await amqp.connect(amqpServer);
     channel = await connection.createChannel();
     await channel.assertQueue("PRODUCT");
 }
-connect();
+connect(); */
 
 app.post("/product/buy", isAuthenticated, async (req, res) => {
-  
-        const { ids } = req.body;
-        const products = await Product.find({ _id: { $in: ids } });
-        channel.sendToQueue(
-            "ORDER",
-            Buffer.from(
-                JSON.stringify({
-                    products,
-                    userEmail: req.user.email,
-                })
-            )
-        );
-        channel.consume("PRODUCT", (data) => {
+    const { ids } = req.body;
+    const products = await Product.find({ _id: { $in: ids } });
+    producer.connect();
+    const message = {
+        value: JSON.stringify({
+            products,
+            userEmail: req.user.email,
+        }),
+    };
+    producer.send({
+        topic: 'ORDER',
+        messages: [message],
+    });
+    //producer.disconnect();
+
+    consumer.connect();
+    consumer.subscribe({ topic: 'PRODUCT', fromBeginning: true });
+    consumer.run({
+        eachMessage: async ({ topic, data }) => {
             order = JSON.parse(data.content);
-        });
-        return res.json(order);
-       
+        },
+    });
+    return res.json(order);
+
 });
+
+
 
 app.post("/product/create", isAuthenticated, async (req, res) => {
     const { name, description, price } = req.body;
